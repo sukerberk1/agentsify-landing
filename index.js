b9f73b13fd12d178115bbdd4e38b33bcfbfc929a
sukerberk1 <szymon.l3232@gmail.com>
2025-01-02 23:01:55 +0100

    Minor changes

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ var revealElements = document.getElementsByClassName("tech-logo");
 for (var i=0; i<revealElements.length; i++) { // create a scene for each element
     new ScrollMagic.Scene({
                         triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
-                        offset: 50,												 // start a little later
+                        offset: ((i % 6) + 1) *10,												 // start a little later
                         triggerHook: 0.9,
                     })
                     .setClassToggle(revealElements[i], "visible") // add class toggle
